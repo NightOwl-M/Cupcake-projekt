b4edd8c08280cc2aeebe4e5d4591e4d3e5bf16a1
Night Owl-M <gittegithub@outlook.dk>
2025-03-25 16:26:59 +0100

    Oprettet kommentarer udfra addRoutes i user og order controllerne.

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -4,7 +4,7 @@ import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 
 public class OrderController {
-
+    // I denne addRoutes metode håndterer vi alle "Handelsrelateret funktioner.
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         //app.get("/", ctx -> backetpage(ctx, connectionPool));
     }
