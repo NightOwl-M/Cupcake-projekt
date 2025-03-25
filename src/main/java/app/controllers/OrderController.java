4657385397de6d8a8b1f0797e76a92db4dd99ed0
Night Owl-M <gittegithub@outlook.dk>
2025-03-25 16:09:17 +0100

    Oprettet addRoutes i orderController.

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,4 +1,11 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
 public class OrderController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        //app.get("/", ctx -> backetpage(ctx, connectionPool));
+    }
 }
