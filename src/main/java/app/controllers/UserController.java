cf1a683be94693bd4aa50b7269ebb90d4ebba109
Night Owl-M <gittegithub@outlook.dk>
2025-03-26 09:49:50 +0100

    Oprettet loginmetoder i DOA.

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,11 +1,16 @@
 package app.controllers;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class UserController {
     // I denne addRoutes metode håndterer vi brugerhåndteringen.
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         //app.post("/login", ctx -> login(ctx, connectionPool));
     }
+
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,4 +1,38 @@
 package app.persistence;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserMapper {
+
+    public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT id, name, balance, isAdmin FROM users WHERE email = ? AND password = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new User(
+                            rs.getInt("id"),
+                            email,
+                            password,
+                            rs.getFloat("balance"),
+                            rs.getBoolean("isAdmin")
+                    );
+                } else {
+                    throw new DatabaseException("Fejl i login. Prøv igen.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("DB fejl");
+        }
+    }
 }
