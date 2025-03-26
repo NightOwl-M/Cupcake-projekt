5b18f94bc18d54e8f2a04a09da2695717e8443d2
William1372 <william.ludwig.oster.dano@gmail.com>
2025-03-26 10:11:27 +0100

    Tilføjet getAllUsers() i UserMapper class

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -2,11 +2,12 @@ package app.persistence;
 
 import app.entities.User;
 import app.exceptions.DatabaseException;
-
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserMapper {
 
@@ -35,4 +36,31 @@ public class UserMapper {
             throw new DatabaseException("DB fejl");
         }
     }
+
+    public static List<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT user_id, user_name, user_password, balance, is_admin FROM public.users";
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    int id = rs.getInt("user_id");
+                    String email = rs.getString("user_name");
+                    String password = rs.getString("user_password");
+                    float balance = rs.getFloat("balance");
+                    boolean isAdmin = rs.getBoolean("is_admin");
+
+                    User user = new User(id, email, password, balance, isAdmin);
+                    users.add(user);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DatabaseException("No users found...");
+        }
+        return users;
+    }
+
 }
